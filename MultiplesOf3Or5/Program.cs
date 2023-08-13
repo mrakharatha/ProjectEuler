65f288818ca21e1200fb7cd44ada57584409939b
seyedmohammadreza <59357443+mrakharatha@users.noreply.github.com>
2023-08-13 10:55:03 +0330

    Add project files.

---
+++ MultiplesOf3Or5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiplesOf3Or5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (MultiplesOf3Or5(i))
+                    sum += i;
+            }
+
+            Console.WriteLine(sum);
+            Thread.Sleep(-1);
+
+        }
+        static bool MultiplesOf3Or5(int number)
+        {
+            return number % 3 == 0 || number % 5 == 0;
+        }
+    }
+}
